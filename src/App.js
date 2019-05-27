51b954bc8c05d985dccca9c4a86a8105c4059969
hunghoang <hunghoang@chotot.vn>
2019-05-27 14:35:47 +0700

    Changed the text in App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>Hello</code> and save to reload.
+            Hello World!!!
           </p>
           <a
             className="App-link"
